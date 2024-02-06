06acc39f84cbda92fc0a3e024ee7ce5500730e0b
ramanzanwar <100433675+ramanzanwar@users.noreply.github.com>
2024-02-06 18:19:10 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -8,7 +8,18 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   const footerMeta = getMetadata('footer');
   block.textContent = '';
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
 
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
   // load footer fragment
   const footerPath = footerMeta.footer || '/footer';
   const fragment = await loadFragment(footerPath);
