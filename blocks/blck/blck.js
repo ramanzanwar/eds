d04d77d419ee9b6848a1b12b7a8c22a30d16ee67
ramanzanwar <100433675+ramanzanwar@users.noreply.github.com>
2024-02-06 17:56:14 +0530

    Update blck.js

--- blocks/blck/blck.js
+++ blocks/blck/blck.js
@@ -3,6 +3,19 @@ export default function decorate(block){
     function onButtonClick() {
   alert('Button clicked!');
 }
+
+    // 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
     const apiUrl = 'http://localhost:8089/actions-service/v1/actions/getUsers';
 
 // Make a GET request
