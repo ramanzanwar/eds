b7852ac74b8cfc82576ec0ee0ba83187263065e7
ramanzanwar <100433675+ramanzanwar@users.noreply.github.com>
2024-02-07 11:59:09 +0530

    Update blck.js

--- blocks/blck/blck.js
+++ blocks/blck/blck.js
@@ -1,38 +1,18 @@
 export default function decorate(block){
-
-    function onButtonClick() {
-  alert('Button clicked!');
-}
-
-    const footerMeta = getMetadata('blck');
-  block.textContent = '';
-    // 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Do Something";
-
-// 2. Append somewhere
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-
-// 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("did something");
-});
     const apiUrl = 'http://localhost:8089/actions-service/v1/actions/getUsers';
-
 // Make a GET request
-fetch(apiUrl)
-.then(response => {
-if (!response.ok) {
-throw new Error('Network response was not ok');
-}
-return response.json();
-})
-.then(data => {
-console.log(data);
-})
-.catch(error => {
-console.error('Error:', error);
-});
+    fetch(apiUrl)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 
 }
