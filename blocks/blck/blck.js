8cb3872964a7674f72a2d9b197508194cf018d8d
ramanzanwar <100433675+ramanzanwar@users.noreply.github.com>
2024-02-20 19:58:07 +0530

    Update blck.js

--- blocks/blck/blck.js
+++ blocks/blck/blck.js
@@ -1,8 +1,28 @@
 export default function decorate(block){
 
-    function onButtonClick() {
-        alert('Button clicked!');
+//     function onButtonClick() {
+//         alert('Button clicked!');
       
+//     const apiUrl = 'http://localhost:8089/actions-service/v1/actions/getUsers';
+
+
+
+// // Make a GET request
+// fetch(apiUrl)
+// .then(response => {
+// if (!response.ok) {
+// throw new Error('Network response was not ok');
+// }
+// return response.json();
+// })
+// .then(data => {
+// console.log(data);
+// })
+// .catch(error => {
+// console.error('Error:', error);
+// });
+//     }
+function Body(){
     const apiUrl = 'http://localhost:8089/actions-service/v1/actions/getUsers';
 
 
@@ -18,8 +38,35 @@ return response.json();
 .then(data => {
 console.log(data);
 })
-.catch(error => {
-console.error('Error:', error);
-});
-    }
+    const DisplayData=data.map(
+        (info)=>{
+            return(
+                <tr>
+                    <td>{info.user_Id}</td>
+                    <td>{info.name}</td>
+                </tr>
+            )
+        }
+    )
+ 
+    return(
+        <div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    </tr>
+                </thead>
+                <tbody>
+                 
+                    
+                    {DisplayData}
+                    
+                </tbody>
+            </table>
+             
+        </div>
+    )
+ }
 }
