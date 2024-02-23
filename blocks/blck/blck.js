dcd6efe01fc702d9f51ffbdddcf588c8ca859e20
Raman Zanwar <raman_zanwar@persistent.com>
2024-02-23 18:28:38 +0530

    Updated code

--- blocks/blck/blck.js
+++ blocks/blck/blck.js
@@ -21,6 +21,7 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
    // Push each response object into the apiResponses array
    newData.push(data);
    console.log('New  data:', newData);
+   console.log(data);
  })
  .catch(error => {
    console.error('Error fetching API data:', error);
